eddfb201f4f34eb25b1e77a271b5654ff26a7b9a
leoacferraz <leoalvo89@gmail.com>
2022-07-02 15:49:04 -0300

    Invocado o método sort diretamente da lista criada "mixedList".

--- 7-days-of-code/src/br/com/sevendaysofcode/myproject/Catalog.java
+++ 7-days-of-code/src/br/com/sevendaysofcode/myproject/Catalog.java
@@ -2,7 +2,6 @@ package br.com.sevendaysofcode.myproject;
 
 import java.io.BufferedWriter;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,7 +26,7 @@ public class Catalog {
 		.flatMap(Collection::stream)
 		.collect(Collectors.toList());
 		
-		Collections.sort(mixedList,Comparator.comparing(Content::title));
+		mixedList.sort(Comparator.comparing(Content::title));
 		
 		new HTMLGenerator(bw).generate(mixedList);
 
